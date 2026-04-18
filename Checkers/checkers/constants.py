0f0d1fc3934c65d6e488879b166b0859f9a4f779
AndrewStephens <andrew.stephens.professional@gmail.com>
2026-04-18 17:05:53 -0600

    First

---
+++ Checkers/checkers/constants.py
@@ -0,0 +1,19 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+#rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
+
+CROWN = pygame.transform.scale(
+    pygame.image.load(r'C:\Users\DrewH\OneDrive\Desktop\CheckersGame\Checkers\assets\pp.png'),
+    (44, 25)
+)
+
+
